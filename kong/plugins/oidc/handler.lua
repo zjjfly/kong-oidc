b121580fd43f6545012e46c1742abd10ab1b5067
Cristian Chiru <cristian.chiru@dcsi.eu>
2022-09-17 10:05:33 +0300

    removed reprecated base_plugin; removed version from rockspec name

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -1,15 +1,10 @@
-local BasePlugin = require "kong.plugins.base_plugin"
-local OidcHandler = BasePlugin:extend()
+local OidcHandler = {
+    PRIORITY = 1000,
+}
 local utils = require("kong.plugins.oidc.utils")
 local filter = require("kong.plugins.oidc.filter")
 local session = require("kong.plugins.oidc.session")
 
-OidcHandler.PRIORITY = 1000
-
-
-function OidcHandler:new()
-  OidcHandler.super.new(self, "oidc")
-end
 
 function OidcHandler:access(config)
   OidcHandler.super.access(self)
